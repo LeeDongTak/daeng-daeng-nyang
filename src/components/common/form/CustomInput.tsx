558afde123981bdfff143d74c2c35fb734e42027
lunaxislu <cmk0905@naver.com>
2024-04-13 16:28:06 +0900

    ✨  customInput create

--- src/components/common/form/CustomInput.tsx
+++ src/components/common/form/CustomInput.tsx
@@ -1,5 +1,33 @@
-const CustomInput = () => {
-  return <div></div>;
+import { ClassValue } from 'clsx';
+import { Control, FieldPath, FieldValues, RegisterOptions } from 'react-hook-form';
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '../../ui/form';
+import { Input } from '../../ui/input';
+
+interface I_ControlProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> {
+  control: Control<TFieldValues>;
+  name: TName;
+  rules?: Omit<RegisterOptions<TFieldValues, TName>, 'valueAsNumber' | 'valueAsDate' | 'setValueAs' | 'disabled'>;
+  classNmae?: ClassValue[];
+}
+const CustomInput = <T extends FieldValues>({ control, name, ...props }: I_ControlProps<T>) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{name}</FormLabel>
+          <FormControl>
+            <Input {...field} {...props} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
 };
 
 export default CustomInput;
