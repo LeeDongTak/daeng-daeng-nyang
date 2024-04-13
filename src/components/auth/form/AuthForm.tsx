dcca2c4ddd0bbfd92a7603f63d2dc72959b5f01b
lunaxislu <cmk0905@naver.com>
2024-04-13 17:37:32 +0900

    ✨ 공통 input button 기능 넣음 #5

--- src/components/auth/form/AuthForm.tsx
+++ src/components/auth/form/AuthForm.tsx
@@ -1,5 +1,19 @@
-const AuthForm = () => {
-  return <div></div>;
+import CustomInput from '@/components/common/form/CustomInput';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { ComponentPropsWithoutRef } from 'react';
+interface I_FormProps extends ComponentPropsWithoutRef<'form'> {
+  children: React.ReactNode;
+}
+const AuthForm = (props: I_FormProps) => {
+  const { className, children } = props;
+  return (
+    <form {...props} className={cn(className)}>
+      {children}
+    </form>
+  );
 };
+AuthForm.input = CustomInput;
+AuthForm.button = Button;
 
 export default AuthForm;
