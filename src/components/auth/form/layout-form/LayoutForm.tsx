a73817423bad578fad20c401ebb7b81601647ff4
lunaxislu <cmk0905@naver.com>
2024-04-14 18:29:04 +0900

    ✅  Form layout develop #5

--- src/components/auth/form/layout-form/LayoutForm.tsx
+++ src/components/auth/form/layout-form/LayoutForm.tsx
@@ -1,5 +1,17 @@
-const LayoutForm = () => {
-  return <div></div>;
+import { Card } from '@/components/ui/card';
+import { Form } from '@/components/ui/form';
+import { ComponentPropsWithoutRef, ReactNode } from 'react';
+import { FieldValues, UseFormReturn } from 'react-hook-form';
+interface I_LayoutFormProps<T extends FieldValues> extends ComponentPropsWithoutRef<'div'> {
+  form: UseFormReturn<T>;
+  children: ReactNode;
+}
+const LayoutForm = <T extends FieldValues>({ form, children, ...props }: I_LayoutFormProps<T>) => {
+  return (
+    <Card {...props}>
+      <Form {...form}>{children}</Form>
+    </Card>
+  );
 };
 
 export default LayoutForm;
