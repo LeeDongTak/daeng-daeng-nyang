e954a6e22c90a40f785286804e9423e4a8869204
lunaxislu <cmk0905@naver.com>
2024-04-14 18:31:02 +0900

    ✅  SignIn Form develop #5

--- src/components/auth/sign-in/SignIn.tsx
+++ src/components/auth/sign-in/SignIn.tsx
@@ -1,11 +1,10 @@
-import { Card } from '@/components/ui/card';
-import { Form } from '@/components/ui/form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import AuthForm from '../form/AuthForm';
-import LayoutFormBody from '../form/layout-form-body/LayoutFormBody';
-import LayoutFormHeader from '../form/layout-form-header/LayoutFormHeader';
+import LayoutForm from '../form/layout-form/LayoutForm';
+import LayoutFormBody from '../form/layout-form/layout-form-body/LayoutFormBody';
+import LayoutFormHeader from '../form/layout-form/layout-form-header/LayoutFormHeader';
 const formSchema = z.object({
   email: z.string().min(2),
   password: z.string().min(2),
@@ -24,18 +23,16 @@ const SignIn = () => {
     console.log(values);
   };
   return (
-    <Form {...form}>
-      <Card>
-        <LayoutFormHeader title="로그인" />
-        <LayoutFormBody>
-          <AuthForm onSubmit={form.handleSubmit(submitHandler)}>
-            <AuthForm.input control={form.control} name="email" />
-            <AuthForm.input control={form.control} name="password" />
-            <AuthForm.button type="submit" text="button" />
-          </AuthForm>
-        </LayoutFormBody>
-      </Card>
-    </Form>
+    <LayoutForm form={form}>
+      <LayoutFormHeader title="로그인" />
+      <LayoutFormBody>
+        <AuthForm onSubmit={form.handleSubmit(submitHandler)}>
+          <AuthForm.input control={form.control} name="email" />
+          <AuthForm.input control={form.control} name="password" />
+          <AuthForm.button type="submit" text="button" />
+        </AuthForm>
+      </LayoutFormBody>
+    </LayoutForm>
   );
 };
 
