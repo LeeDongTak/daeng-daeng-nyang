62f1062c688790af7dc58db8c630c221809061a1
lunaxislu <cmk0905@naver.com>
2024-04-14 19:03:22 +0900

    🔥  Title Component 예시코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,7 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import Title from '@/components/common/Title';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-      <Title level={1} text="Hello world" />
-      <Title level={2} text="Hello world" />
-      <Title level={3} text="Hello world" />
-      <Title level={4} text="Hello world" />
-      <Title level={5} text="Hello world" />
-    </main>
-  );
+  return <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}></main>;
 }
