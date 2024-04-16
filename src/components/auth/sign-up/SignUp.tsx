94f5415d38b2f38601abf92a20f7c8eae03cd76a
lunaxislu <cmk0905@naver.com>
2024-04-16 12:53:56 +0900

    ✨  회원가입 form 작성 #5

--- src/components/auth/sign-up/SignUp.tsx
+++ src/components/auth/sign-up/SignUp.tsx
@@ -1,5 +1,43 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import AuthForm from '../form/AuthForm';
+import LayoutForm from '../form/layout-form/LayoutForm';
+import LayoutFormBody from '../form/layout-form/layout-form-body/LayoutFormBody';
+import LayoutFormHeader from '../form/layout-form/layout-form-header/LayoutFormHeader';
+
+const formSchema = z.object({
+  email: z.string().min(2),
+  password: z.string().min(2),
+  passwordCheck: z.string().min(2),
+});
+
+type T_Schema = z.infer<typeof formSchema>;
 const SignUp = () => {
-  return <div></div>;
+  const form = useForm<T_Schema>({
+    defaultValues: {
+      email: '',
+      password: '',
+      passwordCheck: '',
+    },
+    resolver: zodResolver(formSchema),
+  });
+  const submitHandler = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+  return (
+    <LayoutForm form={form}>
+      <LayoutFormHeader title="회원가입" />
+      <LayoutFormBody>
+        <AuthForm onSubmit={form.handleSubmit(submitHandler)}>
+          <AuthForm.input control={form.control} name="email" />
+          <AuthForm.input control={form.control} name="password" />
+          <AuthForm.input control={form.control} name="passwordCheck" />
+          <AuthForm.button type="submit" text="회원가입" />
+        </AuthForm>
+      </LayoutFormBody>
+    </LayoutForm>
+  );
 };
 
 export default SignUp;
