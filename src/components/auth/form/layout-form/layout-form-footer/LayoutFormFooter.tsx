19f447022402b5cbdd5d09b48a425b4d24d4f779
lunaxislu <cmk0905@naver.com>
2024-04-16 13:02:47 +0900

    ✨  layout-form-footer develop #5

--- src/components/auth/form/layout-form/layout-form-footer/LayoutFormFooter.tsx
+++ src/components/auth/form/layout-form/layout-form-footer/LayoutFormFooter.tsx
@@ -1,5 +1,9 @@
-const LayoutFormFooter = () => {
-  return <div></div>;
+import { CardFooter } from '@/components/ui/card';
+import { ComponentPropsWithoutRef } from 'react';
+
+interface I_FormFooter extends ComponentPropsWithoutRef<'div'> {}
+const LayoutFormFooter = ({ children, ...props }: I_FormFooter) => {
+  return <CardFooter {...props}>{children}</CardFooter>;
 };
 
 export default LayoutFormFooter;
