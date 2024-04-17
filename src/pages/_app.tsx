fc72f7d0f797ad0c9e9030614feb388fcb18b584
LeeDongTak <sunny5796@naver.com>
2024-04-18 00:05:49 +0900

    🔧setting: localFont 적용
    프리텐다드 폰트를 로컬폰트로 적용하였습니다.
    tanstack/react-query설정을 하였습니다. (provider 주입)
    reset.css파일을 삭제하였습니다.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,24 @@
 import Toast from '@/components/common/toast/Toast';
 import Modal from '@/components/modal/Modal';
+import { cn } from '@/lib/utils';
 import '@/styles/globals.css';
-import '@/styles/reset.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
+
+const queryClient = new QueryClient();
+const myFont = localFont({ src: '../../public/font/PretendardVariable.woff2', variable: '--main-font' });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Component {...pageProps} />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <main className={cn(myFont.variable)}>
+        <Component {...pageProps} />
+      </main>
       <Modal />
       <Toast />
-    </>
+    </QueryClientProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
-  return <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}></main>;
+  return <main className={`flex min-h-screen flex-col items-center justify-between p-24`}></main>;
 }
