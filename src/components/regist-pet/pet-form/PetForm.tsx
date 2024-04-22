e22545ae18b6ba9be234e9be776d5f6284698317
lunaxislu <cmk0905@naver.com>
2024-04-23 02:04:31 +0900

    ✨  PetForm 기능 완성 #5

--- src/components/regist-pet/pet-form/PetForm.tsx
+++ src/components/regist-pet/pet-form/PetForm.tsx
@@ -1,5 +1,19 @@
-const PetForm = () => {
-  return <div></div>;
-};
+import CustomInput from '@/components/common/form/input/CustomInput';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { FormHTMLAttributes } from 'react';
 
+interface I_FormProps extends FormHTMLAttributes<HTMLFormElement> {
+  children: React.ReactNode;
+}
+const PetForm = (props: I_FormProps) => {
+  const { className, children } = props;
+  return (
+    <form {...props} className={cn(className)}>
+      {children}
+    </form>
+  );
+};
+PetForm.input = CustomInput;
+PetForm.button = Button;
 export default PetForm;
