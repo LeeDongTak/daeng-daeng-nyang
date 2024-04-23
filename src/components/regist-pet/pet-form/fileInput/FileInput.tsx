3ffbaebb3f86f074470802b1df0b42f488f2bae2
lunaxislu <cmk0905@naver.com>
2024-04-24 01:43:10 +0900

    ✨ fileInput 기능 완성

--- src/components/regist-pet/pet-form/fileInput/FileInput.tsx
+++ src/components/regist-pet/pet-form/fileInput/FileInput.tsx
@@ -1,5 +1,35 @@
-const FileInput = () => {
-  return <div></div>;
+import { FormControl, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Control, FieldPath, FieldValues, RefCallBack, UseFormRegisterReturn } from 'react-hook-form';
+interface I_FileInputProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> {
+  register: {
+    type: 'file';
+    ref: RefCallBack;
+    name: TName;
+    control: Control<TFieldValues>;
+    register: UseFormRegisterReturn<TName>;
+    onChange: (...event: any[]) => void;
+  };
+}
+const FileInput = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({
+  register,
+  ...props
+}: I_FileInputProps<TFieldValues, TName>) => {
+  return (
+    <FormItem>
+      <FormLabel>파일</FormLabel>
+      <FormControl>
+        <Input {...register} className="hidden" />
+      </FormControl>
+      <FormMessage />
+    </FormItem>
+  );
 };
 
 export default FileInput;
