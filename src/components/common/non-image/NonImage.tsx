88eaf345af5e7d4e3ab3b62997a674ee3dea3d6d
lunaxislu <cmk0905@naver.com>
2024-04-24 04:37:13 +0900

    ♻️ NonImage.tsx 컴포넌트 props 수정

--- src/components/common/non-image/NonImage.tsx
+++ src/components/common/non-image/NonImage.tsx
@@ -2,14 +2,14 @@ import { cn } from '@/lib/utils';
 import ImagePlus from '../../../../public/icons/image-plus.svg';
 
 interface I_PropsType {
-  width: string;
-  height: string;
+  className?: string;
 }
-const NonImage = ({ width, height }: I_PropsType) => {
+const NonImage = ({ className }: I_PropsType) => {
   return (
     <div
       className={cn(
-        `flex justify-center items-center w-[${width}] h-[${height}] rounded-[3rem] bg-[#e3eff7] [&_path]:stroke-[#a09bb3]`,
+        `flex justify-center items-center rounded-[3rem] bg-[#e3eff7] [&_path]:stroke-[#a09bb3]`,
+        className,
       )}
     >
       <ImagePlus width={'3.6rem'} height={'3.6rem'} />
