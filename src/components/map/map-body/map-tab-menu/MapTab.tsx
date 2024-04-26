0d1875948010a0b0723ae89240f6276541eb1293
lunaxislu <cmk0905@naver.com>
2024-04-27 00:35:27 +0900

    ✨ tab feature develop #11

--- src/components/map/map-body/map-tab-menu/MapTab.tsx
+++ src/components/map/map-body/map-tab-menu/MapTab.tsx
@@ -1,5 +1,23 @@
+import { Button } from '@/components/ui/button';
+
+const CONTENT_TYPE = [
+  {
+    tab: '병원&약국',
+    api_type: '',
+  },
+  {
+    tab: '산책로',
+    api_type: '',
+  },
+];
 const MapTab = () => {
-  return <div></div>;
+  return (
+    <div>
+      {CONTENT_TYPE.map((content, idx) => (
+        <Button>{content.tab}</Button>
+      ))}
+    </div>
+  );
 };
 
 export default MapTab;
