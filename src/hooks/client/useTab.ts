d799798ef0e8f27264a9aa5b47bcfac6d010bfbb
lunaxislu <cmk0905@naver.com>
2024-04-27 00:49:39 +0900

    ✨  useTab hook develop

--- src/hooks/client/useTab.ts
+++ src/hooks/client/useTab.ts
@@ -1,5 +1,16 @@
-const useTab = () => {
-  return <div></div>;
+import { useState } from 'react';
+
+interface I_UseTabProps<T> {
+  initialValue: number;
+  allTabs: T[];
+}
+const useTab = <T extends { [key: string]: string }>({ initialValue, allTabs }: I_UseTabProps<T>) => {
+  const [currentIndex, setCurrentIndex] = useState<number>(initialValue);
+
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
 };
 
 export default useTab;
