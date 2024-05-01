33a415a6db9481f41c5e0ce6047ec16e718f5ccd
lunaxislu <cmk0905@naver.com>
2024-05-01 20:29:31 +0900

    ✨ map tab 기능 완성 #11

--- src/components/map/map-body/search-location-tab/MapTabs.tsx
+++ src/components/map/map-body/search-location-tab/MapTabs.tsx
@@ -1,5 +1,31 @@
-const MapTabs = () => {
-  return <div></div>;
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+const CONTENT_TYPE = [
+  {
+    tab: '병원&약국',
+    CALL_TYPE: 'hospital',
+  },
+  {
+    tab: '산책로',
+    CALL_TYPE: 'walk',
+  },
+] as const;
+export type T_Search = typeof CONTENT_TYPE;
+const SearchLocationTab = () => {
+  return (
+    <div className="border-b-2 border-[#C5C9CF] ">
+      {CONTENT_TYPE.map((content, idx) => (
+        <Button
+          className={cn(
+            'text-3xl tracking-[0.2rem] bg-transparent text-black  hover:text-white py-8 px-10 hover:border-b-2',
+          )}
+        >
+          {content.tab}
+        </Button>
+      ))}
+    </div>
+  );
 };
 
-export default MapTabs;
+export default SearchLocationTab;
