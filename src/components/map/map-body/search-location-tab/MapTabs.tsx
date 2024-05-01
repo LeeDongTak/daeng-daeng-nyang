3b002726e76830f586d8e0be4f0f31d0f4b63a3b
lunaxislu <cmk0905@naver.com>
2024-05-01 20:35:09 +0900

    ✨ map tab & hook connection #11

--- src/components/map/map-body/search-location-tab/MapTabs.tsx
+++ src/components/map/map-body/search-location-tab/MapTabs.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import useTab from '@/hooks/client/map/search-location/useTab';
 import { cn } from '@/lib/utils';
 
 const CONTENT_TYPE = [
@@ -13,13 +14,16 @@ const CONTENT_TYPE = [
 ] as const;
 export type T_Search = typeof CONTENT_TYPE;
 const SearchLocationTab = () => {
+  const { changeItem, currentIndex } = useTab({ initialValue: 0, allTabs: CONTENT_TYPE });
   return (
     <div className="border-b-2 border-[#C5C9CF] ">
       {CONTENT_TYPE.map((content, idx) => (
         <Button
           className={cn(
             'text-3xl tracking-[0.2rem] bg-transparent text-black  hover:text-white py-8 px-10 hover:border-b-2',
+            currentIndex === idx && 'border-b-2 bg-primary/90 text-white border-b-primary',
           )}
+          onClick={changeItem(idx)}
         >
           {content.tab}
         </Button>
