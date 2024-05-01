90bab56eed5be094bb2436a1f7d0d8986d055b51
lunaxislu <cmk0905@naver.com>
2024-05-01 21:14:29 +0900

    💡 UI컴포넌트 관련 주석 추가 #11
    CustomOverlayMap , CustomMarker의 역활이 무엇인지 주석 처리 함

--- src/components/map/map-body/kakao-map/KakaoMap.tsx
+++ src/components/map/map-body/kakao-map/KakaoMap.tsx
@@ -124,9 +124,12 @@ const KakaoMap = () => {
         onIdle은 맵의 움직임을 동적으로 감지합니다. 따라서 중심좌표의 변경, 줌level등을 동적으로 사용자의 인터렉션에 따라 감지 할수 있습니다. 활용 예시로는 중심좌표가 변화하면 그에 따른 유저가 원하는 location을 중심좌표 주변으로 검색할수 있겠끔 해줄수 있습니다. 
        */
     >
+      {/* 현재 위치 */}
       <CustomOverlayMap position={currentLocation}>
         <MapPin size={48} color="#1f4da8" />
       </CustomOverlayMap>
+
+      {/* location Marker */}
       {markers?.map(marker => {
         return (
           <CustomMarker
