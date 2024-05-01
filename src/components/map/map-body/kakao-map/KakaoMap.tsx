a08d6a160440aa780bb92b0f6dba397ea7ba5e7f
lunaxislu <cmk0905@naver.com>
2024-05-01 21:15:28 +0900

    ✅ 현재 user의 위치로 다시 중심좌표 이동 함수 기능 추가 #11

--- src/components/map/map-body/kakao-map/KakaoMap.tsx
+++ src/components/map/map-body/kakao-map/KakaoMap.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import Skeleton from '@/components/ui/skeleton';
 import useKakaoLoader from '@/hooks/client/map/kakao-map/useKakaoLoader';
 import useKakaoMapStore, {
@@ -46,6 +47,11 @@ const KakaoMap = () => {
     if (!kakaoMap) return;
     const newLatLng = new kakao.maps.LatLng(marker.getPosition().getLat(), marker.getPosition().getLng());
     kakaoMap.panTo(newLatLng);
+  };
+  const clickMoveToUserLocation = () => {
+    if (!kakaoMap) return;
+    const userLocationLatLng = new kakao.maps.LatLng(currentLocation.lat, currentLocation.lng);
+    kakaoMap.setCenter(userLocationLatLng);
   };
   /**
    * 초기 위치값 설정 useEffect
@@ -124,6 +130,7 @@ const KakaoMap = () => {
         onIdle은 맵의 움직임을 동적으로 감지합니다. 따라서 중심좌표의 변경, 줌level등을 동적으로 사용자의 인터렉션에 따라 감지 할수 있습니다. 활용 예시로는 중심좌표가 변화하면 그에 따른 유저가 원하는 location을 중심좌표 주변으로 검색할수 있겠끔 해줄수 있습니다. 
        */
     >
+      <Button onClick={clickMoveToUserLocation}>내 위치</Button>
       {/* 현재 위치 */}
       <CustomOverlayMap position={currentLocation}>
         <MapPin size={48} color="#1f4da8" />
