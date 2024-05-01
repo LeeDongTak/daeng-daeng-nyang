e45449aed49c41c1ad404ba94308f5b47f1e0ebd
lunaxislu <cmk0905@naver.com>
2024-05-01 20:40:26 +0900

    ✅  초기 위치 hook 추가 #11

--- src/components/map/map-body/kakao-map/KakaoMap.tsx
+++ src/components/map/map-body/kakao-map/KakaoMap.tsx
@@ -1,7 +1,8 @@
 import Skeleton from '@/components/ui/skeleton';
 
 import useKakaoLoader from '@/hooks/client/map/kakao-map/useKakaoLoader';
-import { CSSProperties, useState } from 'react';
+import { setCurrentLocation, setCurrentPosition } from '@/store/map/kakako-map/kakaoMap-store';
+import { CSSProperties, useEffect, useState } from 'react';
 import { Map } from 'react-kakao-maps-sdk';
 import CustomMarker from './custom-marker/CustomMarker';
 
@@ -11,6 +12,26 @@ const position = { lat: 33.5563, lng: 126.79581 };
 const KakaoMap = () => {
   const [loading, error] = useKakaoLoader();
   const [map, setMap] = useState<kakao.maps.Map | null>(null);
+  useEffect(() => {
+    /**
+     * 초기 위치값 설정 useEffect
+     */
+    useEffect(() => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+          const { coords } = position;
+          setCurrentPosition({
+            lat: coords.latitude,
+            lng: coords.longitude,
+          });
+          setCurrentLocation({
+            lat: coords.latitude,
+            lng: coords.longitude,
+          });
+        });
+      }
+    }, []);
+  }, []);
   if (loading || error) return <Skeleton type="map" />;
 
   return (
