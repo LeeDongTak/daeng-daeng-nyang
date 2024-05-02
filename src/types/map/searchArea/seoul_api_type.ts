4fe6a66264a598602e558e08da27a5f555cebd58
lunaxislu <cmk0905@naver.com>
2024-05-02 23:02:18 +0900

    ✨ seoul api - 산책로 타입 만듬 #11

---
+++ src/types/map/searchArea/seoul_api_type.ts
@@ -0,0 +1,50 @@
+/**
+ * @explain 서울시 주요 산책로 api type
+    AREA; //부지 면적
+  GUIDANCE; // 안내도
+  G_LATITUDE; // Y좌표(GRS80TM)
+  G_LONGITUDE; //X좌표(GRS80TM)
+  LATITUDE; // Y좌표(WGS84)
+  LONGITUDE; //X좌표(WGS84)
+  MAIN_EQUIP; // 주요시설
+  MAIN_PLANTS; // 주요식물
+  OPEN_DT; // 개원일
+  P_ADDR; // 주소
+  P_ADMINTEL; //전화번호
+  P_IDX; // 연번(공원번호)
+  P_IMG; // 이미지
+  P_LIST_CONTENT; // 공원개요
+  P_NAME; // 관리부서
+  P_PARK; // 공원명
+  P_ZONE; // 지역
+  TEMPLATE_URL; // 홈페이지 바로가기 url
+  USE_REFER; // 이용시 참고사항
+  VISIT_ROAD; // 오시는길
+ */
+
+export interface I_SearchParkInfoService {
+  [key: string]: unknown;
+  row: I_SeoulWalkAPi[];
+}
+export interface I_SeoulWalkAPi {
+  AREA: string; //부지 면적
+  GUIDANCE: string; // 안내도
+  G_LATITUDE: string; // Y좌표(GRS80TM)
+  G_LONGITUDE: string; //X좌표(GRS80TM)
+  LATITUDE: string; // Y좌표(WGS84)
+  LONGITUDE: string; //X좌표(WGS84)
+  MAIN_EQUIP: string; // 주요시설
+  MAIN_PLANTS: string; // 주요식물
+  OPEN_DT: string; // 개원일
+  P_ADDR: string; // 주소
+  P_ADMINTEL: string; //전화번호
+  P_IDX: string; // 연번(공원번호)
+  P_IMG: string; // 이미지
+  P_LIST_CONTENT: string; // 공원개요
+  P_NAME: string; // 관리부서
+  P_PARK: string; // 공원명
+  P_ZONE: string; // 지역
+  TEMPLATE_URL: string; // 홈페이지 바로가기 url
+  USE_REFER: string; // 이용시 참고사항
+  VISIT_ROAD: string; // 오시는길
+}
