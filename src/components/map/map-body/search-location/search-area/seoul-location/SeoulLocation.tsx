d600a2532e0ef111e31ca56f5621d9ae7da41ab9
lunaxislu <cmk0905@naver.com>
2024-05-04 06:06:56 +0900

    💄 조건부 styling 완성  #11

--- src/components/map/map-body/search-location/search-area/seoul-location/SeoulLocation.tsx
+++ src/components/map/map-body/search-location/search-area/seoul-location/SeoulLocation.tsx
@@ -1,20 +1,23 @@
 import { Button } from '@/components/ui/button';
 import { SEOUL_LOCATION } from '@/data/map/seoul-area';
+import useTab from '@/hooks/client/ui/useTab';
 import { cn } from '@/lib/utils';
 const SCROLL_HORIZONTAL_WRAPPER =
   'top-0 -left-[56px] absolute overflow-x-hidden overflow-y-auto origin-top-right w-[60px] h-[450px] -rotate-90 scroll-smooth scrollbar-hide';
 const SCROLL_HORIZONTAL_ELEMENT = 'rotate-90 origin-right-top my-11  w-[60px] h-[24px]';
+const FOCUS_ITEM = 'bg-primary text-white';
 const SeoulLocation = () => {
-  //   'bg-primary text-white';
-
+  const { changeItem, currentIndex } = useTab({ initialValue: null, allTabs: SEOUL_LOCATION });
   return (
     <div className={SCROLL_HORIZONTAL_WRAPPER}>
-      {SEOUL_LOCATION.map(({ location }) => (
+      {SEOUL_LOCATION.map(({ location }, idx) => (
         <Button
           className={cn(
             'bg-transparent text-primary border border-primary rounded-full font-semibold text-lg  leading-relaxedpx-8 hover:text-white ',
             SCROLL_HORIZONTAL_ELEMENT,
+            idx === currentIndex && FOCUS_ITEM,
           )}
+          onClick={changeItem(idx)}
         >
           {location}
         </Button>
--- src/hooks/client/ui/useTab.ts
+++ src/hooks/client/ui/useTab.ts
@@ -1,19 +1,19 @@
 import { useState } from 'react';
 
 interface I_UseTabProps<T> {
-  initialValue: number;
+  initialValue: number | null;
   allTabs: T[];
 }
 const useTab = <T extends { [P in keyof T]: T[P] }>(props: I_UseTabProps<T>) => {
   const { initialValue, allTabs } = props;
-  const [currentIndex, setCurrentIndex] = useState<number>(initialValue);
+  const [currentIndex, setCurrentIndex] = useState<number | null>(initialValue);
 
   const clickChangeItem = (idx: number) => () => {
     setCurrentIndex(idx);
   };
 
   return {
-    currentItem: allTabs[currentIndex],
+    currentItem: allTabs[currentIndex ?? 0],
     changeItem: clickChangeItem,
     currentIndex,
   };
