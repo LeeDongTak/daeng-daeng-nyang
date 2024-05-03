b91dfd57a40c486fb85c7fe71973fe8a64b2e815
lunaxislu <cmk0905@naver.com>
2024-05-04 01:59:23 +0900

    🎉 seoul api 파일 생성 #11

---
+++ src/components/map/api/seoul_api.ts
@@ -0,0 +1,33 @@
+/**
+ * @explain 동물 병원& 약국은 각 지역구 마다 api 호출할 수 있습니다.
+ * @example 동물약국 api
+ * http://openapi.seoul.go.kr:8088/(인증키)/json/LOCALDATA_020302_${지역구}/1/1000/01
+ * @example 동물 병원 api
+ * http://openapi.seoul.go.kr:8088/(인증키)/xml/LOCALDATA_020301_GS/1/5/
+ *
+ * 중구 : JG
+ * 양천구 : YC
+ * 광진구 :GJ
+ * 강동구 : GD
+ * 종로구 : JN
+ * 성동구 : SD
+ * 중랑구 : JR
+ * 금천구 : GC
+ * 강서구 : GS
+ * 영등포구 : YD
+ * 용산구 : YS
+ * 동대문구 : DD
+ * 동작구 : DJ
+ * 강북구 : GB
+ * 노원구 : NW
+ * 송파구 : SP
+ * 서초구 : SC
+ * 서대문구 : SM
+ * 마포구 : MP
+ * 도봉구 : DB
+ * 은평구 : EP
+ * 성북구 : SB
+ * 관악구 : GA
+ * 구로구 : GR
+ * 강남구 : GN
+ */
