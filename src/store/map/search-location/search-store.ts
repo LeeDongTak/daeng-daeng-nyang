b637b53950f174aae28c3170568354be9907b1dc
lunaxislu <cmk0905@naver.com>
2024-05-04 21:36:19 +0900

    ✨  global state add #11

--- src/store/map/search-location/search-store.ts
+++ src/store/map/search-location/search-store.ts
@@ -3,12 +3,15 @@ import { create } from 'zustand';
 interface I_MapSearch {
   category_type: 'hospital' | 'walk'; // 병원&약국 , 산책
   searchValue: string;
+  api_query: null | string;
+  isUsingInnerKakaoApi: boolean;
 }
 
 const initialValue = {
   category_type: 'hospital',
-
   searchValue: '',
+  api_query: null,
+  isUsingInnerKakaoApi: false,
 } as I_MapSearch;
 
 const useSearchLocationStore = create<I_MapSearch>()(() => ({
@@ -21,3 +24,11 @@ export const setSearchType = (category_type: 'hospital' | 'walk') =>
 
 export const setSearchValue = (searchValue: string) =>
   useSearchLocationStore.setState(state => ({ ...state, searchValue }));
+
+export const setApiQuery = (api_query: string | null) =>
+  useSearchLocationStore.setState(state => ({ ...state, api_query }));
+
+export const setIsUsingInnerKakaoApi = (isUsingInnerKakaoApi: boolean) => {
+  if (isUsingInnerKakaoApi === initialValue.isUsingInnerKakaoApi) return;
+  useSearchLocationStore.setState(state => ({ ...state, isUsingInnerKakoApi: isUsingInnerKakaoApi }));
+};
