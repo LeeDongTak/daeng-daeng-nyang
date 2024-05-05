22a43a2364edf85c77e46b92ef2c73280c13c9ff
lunaxislu <cmk0905@naver.com>
2024-05-06 02:04:48 +0900

    ✨  account management 컴포넌트 완성 #55

--- src/components/auth/sign-in/SignIn.tsx
+++ src/components/auth/sign-in/SignIn.tsx
@@ -1,15 +1,11 @@
-import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox';
-import { Label } from '@/components/ui/label';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Fragment } from 'react';
 import { useForm } from 'react-hook-form';
 import LayoutForm from '../../common/form/form-layout/LayoutForm';
 import LayoutFormBody from '../../common/form/form-layout/layout-form-body/LayoutFormBody';
-import LayoutFormHeader from '../../common/form/form-layout/layout-form-header/LayoutFormHeader';
 import AuthForm from '../auth-form/AuthForm';
 import AuthTitle from '../auth-title/AuthTitle';
-import { SignInSchema, T_SignInSchema } from './validator/sign-in-validator';
+import { INPUTS, SignInSchema, T_SignInSchema } from './validator/sign-in-validator';
 
 const SignIn = () => {
   const form = useForm<T_SignInSchema>({
@@ -27,27 +23,18 @@ const SignIn = () => {
     <Fragment>
       <AuthTitle title="Login" subTitle="나의 반려동물을 자랑해보세요" />
       <LayoutForm form={form} className="w-[33.2rem] bg-transparent border-0 shadow-none">
-        <LayoutFormHeader title="" />
         <LayoutFormBody>
           <AuthForm onSubmit={form.handleSubmit(submitHandler)} className="flex flex-col gap-10">
-            <AuthForm.input
-              labelCn="text-xl font-bold"
-              placeholder="아이디를 입력해 주세요."
-              control={form.control}
-              name="email"
-              label="이메일"
-              className="h-[4.8rem] text-xl rounded-2xl pl-7"
-            />
-            <AuthForm.input
-              labelCn="text-xl font-bold"
-              control={form.control}
-              name="password"
-              label="비밀번호"
-              placeholder="비밀번호를 입력해주세요."
-              className="h-[4.8rem] text-xl rounded-2xl pl-7"
-              type="password"
-            />
-
+            {INPUTS.map(input => (
+              <AuthForm.input
+                control={form.control}
+                labelCn="text-xl font-bold"
+                className="h-[4.8rem] text-xl rounded-2xl pl-7"
+                name={input.name}
+                label={input.label}
+                placeholder={input.placeholder}
+              />
+            ))}
             <AuthForm.button
               type="submit"
               className="rounded-full py-8 text-2xl tracking-widest hover:bg-destructive/30 bg-[#E1E6EC] "
@@ -55,17 +42,6 @@ const SignIn = () => {
             >
               로그인
             </AuthForm.button>
-            <div className="flex justify-between items-center">
-              <div className="flex item-center gap-5">
-                <Checkbox id="keep-login" className=" w-[1.4rem] h-[1.4rem] rounded-full" />
-                <Label htmlFor="keep-login" className="text-xl cursor-pointer">
-                  로그인 상태 유지
-                </Label>
-              </div>
-              <Button variant={'link'} className="text-xl " type="button">
-                회원가입하기
-              </Button>
-            </div>
           </AuthForm>
         </LayoutFormBody>
       </LayoutForm>
