70cd21af6c8be60be2934c4cc609a1bffc66beee
lunaxislu <cmk0905@naver.com>
2024-05-07 05:47:39 +0900

    ✨ [Auth] useAuth (client)훅에서 중요한 로직 기능 완성 #61

--- src/hooks/client/auth/useAuth.ts
+++ src/hooks/client/auth/useAuth.ts
@@ -1,5 +1,18 @@
-const useAuth = () => {
-  return <div></div>;
+import { zodResolver } from '@hookform/resolvers/zod';
+import { DefaultValues, FieldValues, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+interface I_UseAuthProps<T extends FieldValues> {
+  schema: z.ZodType<T>;
+  defaultValues: DefaultValues<T>;
+}
+const useAuth = <T extends FieldValues>({ schema, defaultValues }: I_UseAuthProps<T>) => {
+  const form = useForm<T>({
+    defaultValues,
+    resolver: zodResolver(schema),
+  });
+
+  return { form };
 };
 
 export default useAuth;
