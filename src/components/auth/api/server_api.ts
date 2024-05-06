cdc5d954f22caf41d7e7004c857d6bdc64f5f76f
lunaxislu <cmk0905@naver.com>
2024-05-07 05:28:09 +0900

    ✨ [Auth] sign up 함수 기능 만듬 #61

--- src/components/auth/api/server_api.ts
+++ src/components/auth/api/server_api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { T_SignInSchema } from '../sign-in/validator/sign-in-validator';
+import { T_SignUpSchema } from '../sign-up/validator/sign-up-validator';
 
 const baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL;
 
@@ -13,6 +14,15 @@ const axiosApi = axios.create({
 export const submitSignInHandler = async (values: T_SignInSchema) => {
   try {
     const { data } = await axiosApi.post('/auth/sign-in', values);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const submitSignUpHandler = async (values: T_SignUpSchema) => {
+  try {
+    const { data } = await axiosApi.post('/auth/signup', values);
     return data;
   } catch (err) {
     throw err;
