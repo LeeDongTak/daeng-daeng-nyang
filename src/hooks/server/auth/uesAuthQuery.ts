cb033c7b61dc3f665be088ea783dd45deae9ae90
lunaxislu <cmk0905@naver.com>
2024-05-07 04:42:49 +0900

    🔥  불필요 주석 삭제 #61

--- src/hooks/server/auth/uesAuthQuery.ts
+++ src/hooks/server/auth/uesAuthQuery.ts
@@ -15,27 +15,5 @@ const useAuthQuery = <T extends FieldValues>({ schema, defaultValues }: I_useAut
 
   return { form };
 };
-// const useAuthQuery = <T>(schema: z.ZodType<T>, defaultValues: UseFormProps) => {
-//   const form = useForm<T>({
-//     defaultValues,
-//     resolver: zodResolver(schema),
-//   });
-
-//   return { form };
-// };
 
 export default useAuthQuery;
-// const obj = {
-//   // {statusCode: 201, message: '회원가입에 성공했습니다.', user: {…}}
-//   message: '회원가입에 성공했습니다.',
-//   statusCode: 201,
-//   createdAt: '2024-05-06T17:01:44.311Z',
-//   deletedAt: null,
-//   email: 'test@gmail.com',
-//   id: 4,
-//   name: '최문길',
-//   password: '$2b$12$xnLBMM.4zuuYEI/JVVBIKObvZXB.uDE/WDKqV8Wb2.LvnopOmcn3.',
-//   registration_information: 'SITE',
-//   role: 0,
-//   updatedAt: '2024-05-06T17:01:44.311Z',
-// };
