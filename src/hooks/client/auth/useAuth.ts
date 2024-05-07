0a742fb66ab45381a33ccca286f293de32958f29
lunaxislu <cmk0905@naver.com>
2024-05-07 17:54:25 +0900

    ✅ [type] 공통 타입 적용

--- src/hooks/client/auth/useAuth.ts
+++ src/hooks/client/auth/useAuth.ts
@@ -1,15 +1,11 @@
 import { T_SignInSchema } from '@/components/auth/sign-in/validator/sign-in-validator';
 import { T_SignUpSchema } from '@/components/auth/sign-up/validator/sign-up-validator';
 import useAuthQuery from '@/hooks/server/auth/uesAuthQuery';
+import { I_CustomUseHookFormProps } from '@/types/form/form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { DefaultValues, FieldValues, useForm } from 'react-hook-form';
-import { z } from 'zod';
+import { FieldValues, useForm } from 'react-hook-form';
 
-interface I_UseAuthProps<T extends FieldValues> {
-  schema: z.ZodType<T>;
-  defaultValues: DefaultValues<T>;
-}
-const useAuth = <T extends FieldValues>({ schema, defaultValues }: I_UseAuthProps<T>) => {
+const useAuth = <T extends FieldValues>({ schema, defaultValues }: I_CustomUseHookFormProps<T>) => {
   const form = useForm<T>({
     defaultValues,
     resolver: zodResolver(schema),
--- src/hooks/client/pet/pet-regist/usePetRegist.ts
+++ src/hooks/client/pet/pet-regist/usePetRegist.ts
@@ -1,5 +1,6 @@
-const usePetRegist = () => {
-  return <div></div>;
-};
+import { I_CustomUseHookFormProps } from '@/types/form/form';
+import { FieldValues } from 'react-hook-form';
+
+const usePetRegist = <T extends FieldValues>({ schema, defaultValues }: I_CustomUseHookFormProps<T>) => {};
 
 export default usePetRegist;
