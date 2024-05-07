65fafffb00b508bbf274c1c05076698160d73e86
lunaxislu <cmk0905@naver.com>
2024-05-07 17:55:06 +0900

    💡 [comment] 필요 주석

--- src/types/form/form.d.ts
+++ src/types/form/form.d.ts
@@ -1,5 +1,7 @@
 import { DefaultValues, FieldValues } from 'react-hook-form';
-
+/**
+ * 등록 폼에 사용될 커스텀 훅 타입입니다.
+ */
 export interface I_CustomUseHookFormProps<T extends FieldValues> {
   schema: z.ZodType<T>;
   defaultValues: DefaultValues<T>;
