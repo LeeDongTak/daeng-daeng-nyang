bb3a825232b146be398f6f4be85429c7548a33f7
lunaxislu <cmk0905@naver.com>
2024-05-08 21:39:31 +0900

    ✨ [Map/Server] pet 정보 query 훅 완성 #59

--- src/hooks/server/map/useGetPetInfoQueryFetch.ts
+++ src/hooks/server/map/useGetPetInfoQueryFetch.ts
@@ -1,5 +1,20 @@
+import { getUserInfo } from '@/components/map/api/server_api';
+import { useQuery } from '@tanstack/react-query';
+enum QUERY_KEY {
+  FETCH = 'fetch',
+}
+
 const useGetPetInfoQueryFetch = () => {
-  return <div></div>;
+  const { data: pets, error } = useQuery({
+    queryKey: [QUERY_KEY.FETCH],
+    queryFn: getUserInfo,
+    staleTime: Infinity,
+  });
+
+  return {
+    pets,
+    error,
+  };
 };
 
 export default useGetPetInfoQueryFetch;
