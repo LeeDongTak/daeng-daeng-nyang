6d837aa9956db83b7dfe1b4be9ec6a45255f3d12
lunaxislu <cmk0905@naver.com>
2024-05-09 09:48:07 +0900

    ✨ [Map/Server] 변하는 value 모음 컴포넌트 완성 #59

--- src/components/map/modal/MarkerModal.tsx
+++ src/components/map/modal/MarkerModal.tsx
@@ -1,14 +1,13 @@
-import { CALENDAR_CATEGORY } from '@/components/calendar/reservation/reservation-type';
 import LayoutForm from '@/components/common/form/form-layout/LayoutForm';
 import LayoutFormBody from '@/components/common/form/form-layout/layout-form-body/LayoutFormBody';
 import LayoutFormHeader from '@/components/common/form/form-layout/layout-form-header/LayoutFormHeader';
 import useModalForm from '@/hooks/client/map/modal-form/useModalFom';
 import { I_CustomMarkerProps } from '@/types/map/kakao';
 import clsx from 'clsx';
-import { Fragment } from 'react';
 import ScheduleForm from '../form/ScheduleForm';
 import { T_ScheduleSchema, scheduleSchema } from '../form/validator/schedule-validator';
 import DefaultValue from './DefaultValue/DefaultValue';
+import DynamicChangeValue from './DynamicInputValue/DynamicInputValue';
 interface I_MarkerModalProps {
   marker: I_CustomMarkerProps;
   isLogin: boolean;
@@ -32,45 +31,16 @@ const MarkerModal = ({ marker, isLogin }: I_MarkerModalProps) => {
       <LayoutFormHeader title="" descript="" headerCn={{ layoutCn: 'p-5' }} />
       <LayoutFormBody>
         <ScheduleForm onSubmit={form.handleSubmit(submitHandler)} className="flex flex-col gap-10">
+          {/* 절대 변하지 않는 value 컴포넌트 모음집 입니다.  */}
           <DefaultValue isLogin={isLogin} place={marker.place} location={marker.address} />
-          {isLogin && (
-            <Fragment>
-              <div className="flex justify-around">
-                <ScheduleForm.selectBox
-                  control={form.control}
-                  name="petId"
-                  title="나의 펫"
-                  placeholder="펫을 선택해주세요"
-                  optionCn="z-[501]"
-                  labelCn="text-2xl font-semibold"
-                  itemCn="w-[10rem]"
-                  selectItem={select_item}
-                />
-                <ScheduleForm.calendar
-                  control={form.control}
-                  name="date"
-                  calendarLabel="일정을 선택해주세요"
-                  inputCn="w-[12rem]"
-                  className="self-end"
-                  itemCn="self-end"
-                  customDisable={customSelectDisableDate}
-                />
-              </div>
-              <div className="pl-4">
-                <ScheduleForm.radioBox
-                  control={form.control}
-                  name="category"
-                  title="카테고리"
-                  labelCn="text-2xl mb-3 inline-block font-semibold"
-                  radioItem={CALENDAR_CATEGORY}
-                  className="text-lg"
-                />
-              </div>
 
-              <ScheduleForm.button variant={'more'} className="rounded-xl font-normal py-8">
-                일정 등록하기
-              </ScheduleForm.button>
-            </Fragment>
+          {/* 변하는 value 값들 입니다. 로그인 시 선택 할 수 있는 input 들모음집입니다. */}
+          {isLogin && (
+            <DynamicChangeValue
+              control={form.control}
+              select_item={select_item}
+              customSelectDisableDate={customSelectDisableDate}
+            />
           )}
         </ScheduleForm>
       </LayoutFormBody>
