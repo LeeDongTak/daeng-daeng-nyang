4bfb82b6746f2412e13c85ae3ada798e557e633c
hojun.yoon <tjdsksro90@gmail.com>
2024-05-10 14:14:19 +0900

    ✨ update: 스케줄 5개 제한

--- src/components/calendar/modal/body/ModalCalendarBody.tsx
+++ src/components/calendar/modal/body/ModalCalendarBody.tsx
@@ -1,11 +1,14 @@
 import { Button } from '@/components/ui/button';
 import { useModal } from '@/hooks/client/ui/useModal';
+import useToast from '@/hooks/client/useToast';
+import useScheduleListStore from '@/store/calendar/schedule-store';
 import RegistCalendar from '../../form/RegistCalendar';
 import ModalSchedule from '../schedule/ModalSchedule';
 
 const ModalCalendarBody = () => {
+  const { toast } = useToast();
   const { DaengModal } = useModal();
-
+  const scheduleListData = useScheduleListStore(state => state.scheduleListData);
   return (
     <div>
       <ModalSchedule />
@@ -13,6 +16,15 @@ const ModalCalendarBody = () => {
         className="w-full mt-2"
         type="button"
         onClick={() => {
+          if (scheduleListData.length > 4) {
+            toast({
+              title: '하루에 일정 최대 갯수는 5개입니다.',
+              variant: 'danger',
+              position: 'top-center',
+              closeTimeOut: 2000,
+            });
+            return;
+          }
           DaengModal.fire(<RegistCalendar />);
         }}
       >
