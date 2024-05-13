104c22663854c8823b225a125c6c00cf27b08b94
LeeDongTak <sunny5796@naver.com>
2024-05-13 21:05:26 +0900

    stting: next.config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,14 @@ const nextConfig = {
 
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/v2/:path*/',
+        destination: `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/:path*/`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
