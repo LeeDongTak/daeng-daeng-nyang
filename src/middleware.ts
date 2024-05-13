7643130e9dfe7d3ec49f3add1e564ceab3d227c4
lunaxislu <cmk0905@naver.com>
2024-05-13 13:34:34 +0900

    ✨ [Auth/Server] middleware로 처리 완성 #64

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { getToken } from 'next-auth/jwt';
+import { NextRequest } from 'next/server';
+
+const ROOT = '/';
+const RESTRIC_ROUTE = ['/calendar', '/gallery/add', '/profile'];
+const DEFAULT_REDIRECT = '/auth/login';
+
+export default async function middleware(req: NextRequest) {
+  const { nextUrl } = req;
+  const token = await getToken({ req }); // client:X, Server:O
+
+  const isAuthenticated = !!token;
+  const isPublicRoute = RESTRIC_ROUTE.includes(nextUrl.pathname);
+  console.log('🚀 ~ middleware ~ isPublicRoute:', isPublicRoute);
+
+  // 로그인시 auth/login으로 넘어가지 않게 하기
+  if (isAuthenticated && !isPublicRoute) return Response.redirect(new URL(ROOT, req.url));
+
+  if (!isAuthenticated && isPublicRoute) return Response.redirect(new URL(DEFAULT_REDIRECT, req.url));
+}
+
+export const config = {
+  matcher: ['/auth/login', '/calendar', '/gallery/add', '/profile'],
+};
