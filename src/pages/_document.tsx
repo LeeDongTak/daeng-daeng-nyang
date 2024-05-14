a753d99c0333a8c07b4c7101dc2259adac85c1de
lunaxislu <cmk0905@naver.com>
2024-05-14 15:31:17 +0900

    🐛 [Bug/Map] 5-2차 시도 #104

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head />
-      {/* <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" /> */}
+      <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       <body>
         <Main />
         <NextScript />
