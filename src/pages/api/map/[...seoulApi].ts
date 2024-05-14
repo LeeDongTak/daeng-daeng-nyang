527ada2fb0dfe97e67efbb6adf2a7521c809f903
lunaxislu <cmk0905@naver.com>
2024-05-14 18:16:12 +0900

    🐛 [Bug/Map] 9차시도 #104

--- src/pages/api/map/[...seoulApi].ts
+++ src/pages/api/map/[...seoulApi].ts
@@ -4,14 +4,14 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function POST(req: NextApiRequest, res: NextApiResponse) {
   const { seoulApi } = req.query;
   const { api_name, query_key, api_query } = req.body;
-
+  console.log(`${seoulApi![2]}/${seoulApi![3]}/${seoulApi![4]}/${seoulApi![5]}`);
   const api_fn = {
     animal_hospital: animalHospitalAPI,
     animal_pharmacy: animalPharamcyAPI,
   };
   try {
     const result = await api_fn[api_name as 'animal_hospital' | 'animal_pharmacy'](
-      `${seoulApi![1]}/${seoulApi![2]}/${seoulApi![3]}`,
+      `${seoulApi![2]}${seoulApi![3]}/${seoulApi![4]}/${seoulApi![5]}/`,
     );
     res.status(200).send({ data: result.data, query_string: query_key, api_query });
   } catch (err) {
