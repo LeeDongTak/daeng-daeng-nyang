3c728272df6286d90f373816a2a8b662468a13a7
lunaxislu <cmk0905@naver.com>
2024-05-17 09:21:39 +0900

    🐛 [Bug/Map]  데이터 오류 처리 #126
    seoul API에서 데이터 수정을 안해줍니다. 데이터 삭제를 하드코딩 했습니다.

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,10 +1,6 @@
 import Map from '@/components/map/Map';
 import { searchSeoulParkInfo } from '@/components/map/api/seoul_api';
-import {
-  refinedHashMap,
-  removeEmptySeoulParkInfoHashMap,
-  replaceLocationToApiQuery,
-} from '@/components/map/utility/map-utils';
+import { examineSeoulParkHashMap, refinedHashMap, replaceLocationToApiQuery } from '@/components/map/utility/map-utils';
 import { formattedGroupByKey } from '@/lib/utils';
 import { setSeoulPark } from '@/store/map/seoul-park/seoulPark-store';
 import { I_SearchParkInfoService, I_SeoulParkAPI } from '@/types/map/searchArea/seoul_api_type';
@@ -21,7 +17,7 @@ const MapPage = ({ seoulParkInfo }: { seoulParkInfo: I_SeoulParkAPI[] }) => {
   // 데이터를 해시맵으로 정형화 함
   const formattedData = formattedGroupByKey<I_SeoulParkAPI>(seoulParkInfo, 'P_ZONE');
   // 서울 공원에서 location이 빈문자열이기에 빈문자열 데이터 처리
-  const seoulParkInfoHashMap = removeEmptySeoulParkInfoHashMap(formattedData);
+  const seoulParkInfoHashMap = examineSeoulParkHashMap(formattedData);
   // 기존 api 호출과 통일성 같게 하기 위해 처리함수
   const refinedParkInfo = replaceLocationToApiQuery(seoulParkInfoHashMap);
   // marker로 들어가는 배열로 변환 작업하는 함수
