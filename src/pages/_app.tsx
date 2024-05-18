017506a1cf75f67a231452f38bed9bc0fcd91118
lunaxislu <cmk0905@naver.com>
2024-05-18 15:01:03 +0900

    ✨ [feat/auth/perfetch] app 기능 추가 #102

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,9 @@
 import CommonLayout from '@/components/common/layout/CommonLayout';
 import Toast from '@/components/common/toast/Toast';
-
 import Modal from '@/components/modal/Modal';
 import { cn } from '@/lib/utils';
 import '@/styles/globals.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { HydrationBoundary, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
@@ -13,19 +12,21 @@ import localFont from 'next/font/local';
 const queryClient = new QueryClient();
 const myFont = localFont({ src: '../../public/font/PretendardVariable.woff2', variable: '--main-font' });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <SessionProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <main className={cn(myFont.variable)}>
-          <CommonLayout>
-            <Component {...pageProps} />
-          </CommonLayout>
-        </main>
-        <Modal />
-        <Toast />
-      </SessionProvider>
-    </QueryClientProvider>
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
+        <HydrationBoundary state={pageProps.dehydratedState}>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <main className={cn(myFont.variable)}>
+            <CommonLayout>
+              <Component {...pageProps} />
+            </CommonLayout>
+          </main>
+          <Modal />
+          <Toast />
+        </HydrationBoundary>
+      </QueryClientProvider>
+    </SessionProvider>
   );
 }
