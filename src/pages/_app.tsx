5023065d70005a435df2120cfd8b005078f575ff
lunaxislu <cmk0905@naver.com>
2024-05-18 16:38:39 +0900

    ✨ [feat/auth/prefetch] prefetch #102
    queryClient 사용 안해도 된다 생각 해서 queryClient의 prefetch는 하지 않음

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import Toast from '@/components/common/toast/Toast';
 import Modal from '@/components/modal/Modal';
 import { cn } from '@/lib/utils';
 import '@/styles/globals.css';
-import { HydrationBoundary, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
@@ -16,16 +16,14 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
   return (
     <SessionProvider session={session}>
       <QueryClientProvider client={queryClient}>
-        <HydrationBoundary state={pageProps.dehydratedState}>
-          <ReactQueryDevtools initialIsOpen={false} />
-          <main className={cn(myFont.variable)}>
-            <CommonLayout>
-              <Component {...pageProps} />
-            </CommonLayout>
-          </main>
-          <Modal />
-          <Toast />
-        </HydrationBoundary>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <main className={cn(myFont.variable)}>
+          <CommonLayout>
+            <Component {...pageProps} />
+          </CommonLayout>
+        </main>
+        <Modal />
+        <Toast />
       </QueryClientProvider>
     </SessionProvider>
   );
