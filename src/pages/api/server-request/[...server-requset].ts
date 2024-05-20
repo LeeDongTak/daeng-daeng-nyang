70766f5d577752fce4bd15252e7a1aaa64feb584
LeeDongTak <sunny5796@naver.com>
2024-05-20 15:57:23 +0900

    fix: 버그 수정

--- src/pages/api/server-request/[...server-requset].ts
+++ src/pages/api/server-request/[...server-requset].ts
@@ -1,3 +1,4 @@
+import { axiosAPI } from '@/api/common/axios_instance';
 import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 
@@ -12,20 +13,23 @@ export default async function serverRequest(req: NextApiRequest, res: NextApiRes
   console.log(authorization);
   console.log(isNotHeader);
   try {
+    // path검사
     if (!url) {
       res.status(200).send('url을 입력해 주세요');
       return;
     }
-    if (!authorization || !refreshtoken) {
-      res.status(200).send('토큰이 없습니다. ');
-      return;
-    }
-    // 외부 API로 통신
-    if (Boolean(isNotHeader)) {
+    // 토큰 없는 api 서버 통신
+    if (Boolean(isNotHeader) || method === 'DELETE') {
       const response = await axiosAPI.get(`${url}`);
       res.status(200).send(response.data);
       return;
     }
+    // 토큰 검사
+    if (!authorization || !refreshtoken) {
+      res.status(200).send('토큰이 없습니다. ');
+      return;
+    }
+    // api 서버 통신
     const config = {
       method,
       url: `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}${url}`,
