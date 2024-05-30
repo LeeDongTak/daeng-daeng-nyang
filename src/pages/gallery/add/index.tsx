194f20c82304ba0d29f6c53fe3277cfe7b9fb1e2
LeeDongTak <sunny5796@naver.com>
2024-05-30 20:32:25 +0900

    bugfix: 갤러리 등록 버그 해결중

--- src/pages/gallery/add/index.tsx
+++ src/pages/gallery/add/index.tsx
@@ -1,22 +1,13 @@
 import GalleryRegist from '@/components/gallery/gallery-form/GalleryRegist';
-import { I_GalleryData } from '@/components/gallery/type/gallery';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 
 const AddGalleryPage = () => {
-  const router = useRouter();
-
-  const handleAddGallery = (newGallery: I_GalleryData) => {
-    // 갤러리 페이지로 이동
-    router.push('/gallery');
-  };
-
   return (
     <>
       <Head>
         <title>댕댕냥 - 갤러리 등록</title>
       </Head>
-      <GalleryRegist onAddGallery={handleAddGallery} />
+      <GalleryRegist />
     </>
   );
 };
