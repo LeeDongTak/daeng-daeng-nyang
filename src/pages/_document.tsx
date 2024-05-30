c77530703468ab1fdeb176dbb785aae58e35d84f
lunaxislu <cmk0905@naver.com>
2024-05-30 17:21:57 +0900

    🚚 [SEO] manifest.json 경로 이전 #137
    /favicon/manifest.json일때 404 error 뜸
    
    -> /manifest.json으로 경로 바꿔주니 해결

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,7 +25,7 @@ export default function Document() {
           property="og:description"
           content="반려동물에게 필요한 동물병원, 동물약국 그리고 산책로에대한 정보를 한 눈에 확인하고, 일정을 등록해보세요!"
         />
-        <meta property="og:image" content={'/image/seo/OpenGraph-Image-1200x620.png'} />
+        <meta property="og:image" content={'/seo/open-graph/OpenGraph-Image-1200x620.png'} />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="620" />
         <meta property="og:image:type" content="image/png" />
@@ -52,7 +52,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="32x32" href="/seo/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="96x96" href="/seo/favicon/favicon-96x96.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/seo/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/seo/faviocn/manifest.json" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-TileImage" content="/seo/favicon/ms-icon-144x144.png" />
         <meta name="theme-color" content="#ffffff" />
