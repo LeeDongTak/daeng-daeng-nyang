7a21352d0fdaff76666675f5fe8896160d02dd85
LeeDongTak <sunny5796@naver.com>
2024-06-05 01:25:12 +0900

    fix: querystring을 제거하기 위한 코드

--- src/pages/api/server-request/[...server-requset].ts
+++ src/pages/api/server-request/[...server-requset].ts
@@ -11,21 +11,21 @@ import { getToken } from 'next-auth/jwt';
 
 export default async function serverRequest(req: NextApiRequest, res: NextApiResponse) {
   const body = req.body;
-
   const method = req.method;
   const session = await getToken({ req });
-  const url = req.url?.replace('/api/server-request/', '');
   const { isNotHeader } = req.query as { isNotHeader: string };
 
+  const urlInQueryString = req.url?.replace('/api/server-request/', '') as string;
+  const url = urlInQueryString.substring(0, urlInQueryString.indexOf('?', 0));
   try {
     // path검사
-    if (!url) {
+    if (!urlInQueryString) {
       res.status(200).send('url을 입력해 주세요');
       return;
     }
     // 토큰 없는 api 서버 통신
     if (Boolean(isNotHeader)) {
-      const response = await axiosAPI.get(`${url}`);
+      const response = await axiosAPI.get(`${url ? url : urlInQueryString}`);
       res.status(200).send(response.data);
       return;
     }
@@ -34,10 +34,11 @@ export default async function serverRequest(req: NextApiRequest, res: NextApiRes
       res.status(200).send('토큰이 없습니다. ');
       return;
     }
+        url: `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}${url ? url : urlInQueryString}`,
     // api 서버 통신
     const config = {
       method,
-      url: `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}${url}`,
+      url: `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}${url ? url : urlInQueryString}`,
       headers: {
         Authorization: `Bearer ${session.accessToken}`,
         refreshToken: `${session.refreshtoken}`,
