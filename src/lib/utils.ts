8cb662ce409616624e96aab57f2dd897dd1dbee5
LeeDongTak <sunny5796@naver.com>
2024-06-05 19:18:08 +0900

    update:토큰이 없으면 로그아웃하는 함수

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import { clsx, type ClassValue } from 'clsx';
+import { signOut } from 'next-auth/react';
 
 import { twMerge } from 'tailwind-merge';
 
@@ -27,3 +29,16 @@ export function formattedGroupByKey<T extends { [key: string | number]: unknown 
     return acc;
   }, new Map<keyof T, T[]>());
 }
+
+/**
+ * 토큰이 없다는 에러가 올 시 로그인 페이지로 리다이렉션하는 함수
+ */
+export const RedirectLoginPage = async (isError: Boolean, error: Error | null) => {
+  if (!isError || !error) return false;
+  if (!axios.isAxiosError(error)) return false;
+  if (error.response?.data.message.includes('accessToken') || error.response?.data.message.includes('토큰')) {
+    await signOut();
+    return true;
+  }
+  return false;
+};
