1390cbfbd19c3989a73f35a7e6b35a9a0c277ea2
LeeDongTak <sunny5796@naver.com>
2024-06-05 17:21:15 +0900

    fix: 펫등록시 파일 부분 형식에 맟추어서 변경

--- src/components/regist-pet/api/server_api.ts
+++ src/components/regist-pet/api/server_api.ts
@@ -1,9 +1,16 @@
 import { axiosValid_API } from '@/api/common/axios_instance';
+import { getBase64 } from '@/lib/utils';
 import { T_PetRegistSchema } from '../validator/pet-regist-validator';
 
 export const addPetInfo = async (values: T_PetRegistSchema) => {
   try {
-    const { data } = await axiosValid_API.post('/pet', values);
+    const { data } = await axiosValid_API.post(`/pet?dataType=formData`, {
+      ...values,
+      file: {
+        file: getBase64(values.file as File),
+        fileName: values.file?.name ?? '',
+      },
+    });
     return data;
   } catch (error) {
     throw error;
