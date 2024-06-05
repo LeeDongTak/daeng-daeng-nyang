79ee0af4ab6b85074207492cb5d5c7c827f957ee
LeeDongTak <sunny5796@naver.com>
2024-06-05 19:19:03 +0900

    fix: metaTag 추가

--- src/pages/gallery/edit/[id].tsx
+++ src/pages/gallery/edit/[id].tsx
@@ -1,7 +1,19 @@
 import GalleryUpdate from '@/components/gallery/GalleryUpdate';
+import Head from 'next/head';
 
 const GalleryUpdatePage = () => {
-  return <GalleryUpdate />;
+  return (
+    <>
+      <Head>
+        <title>갤러리 홈 - 댕댕냥 </title>
+        <meta name="author" content="냥댕댕" />
+        <meta name="title" content="갤러리 홈 - 댕댕냥이 세상을 구한다!!" />
+        <meta name="description" content="반려동물과 함께한 일상을 확인해보세요." />
+        <meta name="keywords" content="반려동물, 애완동물, 산책, 동물병원, 동물약국, 산책로, 반려동물 산책" />
+      </Head>
+      <GalleryUpdate />
+    </>
+  );
 };
 
 export default GalleryUpdatePage;
