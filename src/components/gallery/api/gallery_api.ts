ef6704deca0ac06b1f0a428a837891eb2c5a17bc
LeeDongTak <sunny5796@naver.com>
2024-06-05 19:19:25 +0900

    fix: formdata형식에 맟게 수정

--- src/components/gallery/api/gallery_api.ts
+++ src/components/gallery/api/gallery_api.ts
@@ -1,6 +1,6 @@
 import { axiosValid_API } from '@/api/common/axios_instance';
 
 export const updateGallery = async ({ id, formData }: { id: string; formData: FormData }) => {
-  const response = await axiosValid_API.patch(`post/${id}`, formData);
+  const response = await axiosValid_API.patch(`post/${id}?dataType=formData`, formData);
   return response.data;
 };
