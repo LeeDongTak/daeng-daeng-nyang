8bf53752da92b60b1547e5b77f32b8ed5125a601
LeeDongTak <sunny5796@naver.com>
2024-06-06 18:39:31 +0900

    upate: 이미지를 get요청하는 api route작성

--- src/pages/api/server-request/[...server-requset].ts
+++ src/pages/api/server-request/[...server-requset].ts
@@ -4,19 +4,28 @@ import FormData from 'form-data';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getToken } from 'next-auth/jwt';
 
-
 export default async function serverRequest(req: NextApiRequest, res: NextApiResponse) {
   const body = req.body;
   const method = req.method;
   const session = await getToken({ req });
   const urlInQueryString = req.url?.replace('/api/server-request/', '') as string;
   const url = urlInQueryString.substring(0, urlInQueryString.indexOf('?', 0));
-  const { isNotHeader, dataType } = req.query as { isNotHeader: string; dataType: string };
+  const { isNotHeader, dataType, isImage } = req.query as { isNotHeader: string; dataType: string; isImage: string };
+  console.log(body);
 
   try {
     // path검사
     if (!urlInQueryString) {
       res.status(200).send('url을 입력해 주세요');
+      return;
+    }
+    // 이미지 데이터 가져오기
+    if (Boolean(isImage)) {
+      const response = await axios.get(`${url ? url : urlInQueryString}`, { responseType: 'arraybuffer' });
+      const dataBuffer = Buffer.from(response.data);
+      const ext = url.split('.').pop();
+      res.setHeader('Content-Type', `image/${ext}`);
+      res.status(200).send(dataBuffer);
       return;
     }
     // 토큰 없는 api 서버 통신
@@ -38,6 +47,7 @@ export default async function serverRequest(req: NextApiRequest, res: NextApiRes
           (body[key].file || body[key].fileName) &&
           (typeof body[key].file === 'object' || typeof body[key].fileName === 'object')
         ) {
+          console.log(method);
           const base64Data = body.images.file.map((image: string) => image.replace(/^data:image\/\w+;base64,/, ''));
           const buffer = base64Data.map((base64: string) => Buffer.from(base64, 'base64'));
           formData.append('thumbnail', buffer[0], { filename: body.images.fileName[0] });
@@ -70,6 +80,7 @@ export default async function serverRequest(req: NextApiRequest, res: NextApiRes
         },
         data: formData,
       };
+      console.log(formData);
       const response = await axios(config);
       res.status(200).send(response.data);
       return;
@@ -90,6 +101,7 @@ export default async function serverRequest(req: NextApiRequest, res: NextApiRes
     const response = await axios(result);
     res.status(200).send(response.data);
   } catch (error) {
+    console.log(error);
     if (axios.isAxiosError(error) && error.response) {
       res.status(error.response.data.statusCode).send(error.response.data);
     } else {
