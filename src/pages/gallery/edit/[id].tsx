d6cdb7dd9407f2375865b8b9057858da20b1a5a9
LeeDongTak <sunny5796@naver.com>
2024-06-06 18:40:09 +0900

    fix: meta title수정

--- src/pages/gallery/edit/[id].tsx
+++ src/pages/gallery/edit/[id].tsx
@@ -5,7 +5,7 @@ const GalleryUpdatePage = () => {
   return (
     <>
       <Head>
-        <title>갤러리 홈 - 댕댕냥 </title>
+        <title>갤러리 수정 - 댕댕냥 </title>
         <meta name="author" content="냥댕댕" />
         <meta name="title" content="갤러리 홈 - 댕댕냥이 세상을 구한다!!" />
         <meta name="description" content="반려동물과 함께한 일상을 확인해보세요." />
